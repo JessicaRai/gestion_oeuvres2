4ab702034aa57ac5c8c6bd71b5718d10b83f9fb1
JessicaRai <jessicaakle3@gmail.com>
2025-03-22 14:19:19 +0100

    Ajout des modèles migrations et controllers

---
+++ app/Http/Controllers/OeuvreController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Oeuvre;
+use Illuminate\Http\Request;
+
+class OeuvreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Oeuvre $oeuvre)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Oeuvre $oeuvre)
+    {
+        //
+    }
+}
